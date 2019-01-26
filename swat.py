9624826e382481fa2488d6994b0d1b3eee1e0a09
Hiroshi <44738441+rootmeskids@users.noreply.github.com>
2019-01-26 15:25:14 -0500

    Update swat.py

--- swat.py
+++ swat.py
@@ -1,7 +1,9 @@
 import socket
 import threading
+import requests
 import os
 import time
+from bs4 import BeautifulSoup
 from queue import Queue
 
 print(" ______     __     __     ______     ______  ")
@@ -9,9 +11,9 @@ print("/\  ___\   /\ \  _ \ \   /\  __ \   /\__  _\ ")
 print('\ \___  \  \ \ \/ ".\ \  \ \  __ \  \/_/\ \/ ')
 print(' \/\_____\  \ \__/".~\_\  \ \_\ \_\    \ \_\ ')
 print("  \/_____/   \/_/   \/_/   \/_/\/_/     \/_/ ")
-print("Security. Web.Analysis. Tool\n\n[1]H2IP\n[2]PScan\n[3]Scarab\n[h]Help")
+print("Security. Web.Analysis. Tool\n\n[1]H2IP\n[2]PScan\n")
 print("Select Program")
-x = int(input(">>>"))
+x = input(">>>")
 
 if x == '1':
     os.system('cls')
@@ -36,7 +38,7 @@ if x == '1':
     #print(results)
 else:
  print("[!]Error[!]")
- 
+
 if x == '2':
  print_lock = threading.Lock()
  os.system('cls')
@@ -83,42 +85,39 @@ else:
  print("[!]Error[!]")
 
 if x == '3':
- os.system('cls')
- print("   _____                       __  ")
- print("  / ___/_________ __________ _/ /_ ")
- print("  \__ \/ ___/ __ `/ ___/ __ `/ __ \")
- print(" ___/ / /__/ /_/ / /  / /_/ / /_/ /")
- print("/____/\___/\__,_/_/   \__,_/_.___/ ")
- print("Crawl The Web...catch your prey\n")
- print("Enter Site To Crawl")
- xs = input(">>>")
+    os.system('cls')
+    print("   _____                       __  ")
+    print("  / ___/_________ __________ _/ /_ ")
+    print("  \__ \/ ___/ __ `/ ___/ __ `/ __ \ ")
+    print(" ___/ / /__/ /_/ / /  / /_/ / /_/ /")
+    print("/____/\___/\__,_/_/   \__,_/_.___/ ")
+    print("Crawl The Web...catch your prey\n")
+    print("Enter Site To Crawl")
+    xs = input(">>>")
+    page = requests.get(xs)
+    soup = BeautifulSoup(page.text, 'html.parser')
 
- page = requests.get(xs)
- soup = BeautifulSoup(page.text, 'html.parser')
+    last_links = soup.find(class_='AlphaNav')
+    last_links.decompose()
+    artist_name_list = soup.find(class_='BodyText')
+    artist_name_list_items = artist_name_list.find_all('a')
 
- last_links = soup.find(class_='AlphaNav')
- last_links.decompose()
- artist_name_list = soup.find(class_='BodyText')
- artist_name_list_items = artist_name_list.find_all('a')
-
- names = artist_name.contents[0]
-	 print(names)
-	
- for artist_name in artist_name_list_items:
-	 names = artist_name.contents[0]
-	 print(names)
+    for artist_name in artist_name_list_items:
+     names = artist_name.contents[0]
+     print(names)
 else:
-  print("[!]Error has occured")
- if x == 'h':
-  print("______  __    ______        ")
-  print("___  / / /_______  /_______ ")
-  print("__  /_/ /_  _ \_  /___  __ \")
-  print("_  __  / /  __/  / __  /_/ /")
-  print("/_/ /_/  \___//_/  _  .___/ ")
-  print("                   /_/      \n\n")
-  print("[h] - Displays Help Menu\n")
-  print("[1]/H21P - Gives the IP address for a specified URL\n")
-  print("[2]/PScan - Scans for open ports on specified website/ip address")
-  print("[3]/Scarab - Crawls a specified website and outputs items")
- else:
-  print("[!]Error has occured")
+ print("[!]Error has occured")
+
+if x == 'h':
+    print("______  __    ______        ")
+    print("___  / / /_______  /_______ ")
+    print("__  /_/ /_  _ \_  /___  __ \ ")
+    print("_  __  / /  __/  / __  /_/ /")
+    print("/_/ /_/  \___//_/  _  .___/ ")
+    print("                   /_/      \n\n")
+    print("[h] - Displays Help Menu\n")
+    print("[1]/H21P - Gives the IP address for a specified URL\n")
+    print("[2]/PScan - Scans for open ports on specified website/ip address")
+    print("[3]/Scarab - Crawls a specified website and outputs items")
+else:
+ print("[!]Error has occured")
